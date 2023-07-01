c5fbf3fb854b196e1d4e45939461fd72ec1edd85
ArshiaSaleem98 <arshiasaleem98@gmail.com>
2023-07-01 19:47:45 +0100

    feat:frontend-setup-installation

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
+  },
+};
