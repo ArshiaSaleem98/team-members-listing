bbc181690f44830ce7e7283133f402e414da1dbf
Arshia <arshia@MacBook-Pro.broadband>
2023-07-04 20:24:42 +0100

    feat:add-backend-validation

--- backend/models/membersModel.js
+++ backend/models/membersModel.js
@@ -17,79 +17,100 @@ const getAllMembers = () => {
 // Creating a new member
 const createAMember = (data) => {
   return new Promise((resolve, reject) => {
-    db.run(
-      'INSERT INTO Member (name, teamId) VALUES (?, ?)',
-      [data.name, data.teamId],
-      function (error) {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(this.lastID);
+    if (!data.name || !data.teamId) {
+      reject('Member name and team ID are required.');
+    } else {
+      db.run(
+        'INSERT INTO Member (name, teamId) VALUES (?, ?)',
+        [data.name, data.teamId],
+        function (error) {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(this.lastID);
+          }
         }
-      }
-    );
+      );
+    }
   });
 };
 
-// Getting a member by id
+// Getting a member by ID
 const getAMemberById = (id) => {
   return new Promise((resolve, reject) => {
-    db.get('SELECT * FROM Member WHERE id = ?', [id], (error, row) => {
-      if (error) {
-        reject(error);
-      } else if (!row) {
-        resolve(null); // if Member with given id is not found
-      } else {
-        resolve(row);
-      }
-    });
+    if (!id) {
+      reject('Member ID is required.');
+    } else {
+      db.get('SELECT * FROM Member WHERE id = ?', [id], (error, row) => {
+        if (error) {
+          reject(error);
+        } else if (!row) {
+          resolve(null); // if Member with given ID is not found
+        } else {
+          resolve(row);
+        }
+      });
+    }
   });
 };
 
-// Updating a member with id and updated data
+// Updating a member with ID and updated data
 const updateAMember = (id, data) => {
   return new Promise((resolve, reject) => {
-    db.run(
-      'UPDATE Member SET name = ?, teamId = ? WHERE id = ?',
-      [data.name, data.teamId, id],
-      function (error) {
-        if (error) {
-          reject(error);
-        } else if (this.changes === 0) {
-          resolve(null); // if member is not found
-        } else {
-          resolve(id);
+    if (!id) {
+      reject('Member ID is required.');
+    } else if (!data.name || !data.teamId) {
+      reject('Member name and team ID are required.');
+    } else {
+      db.run(
+        'UPDATE Member SET name = ?, teamId = ? WHERE id = ?',
+        [data.name, data.teamId, id],
+        function (error) {
+          if (error) {
+            reject(error);
+          } else if (this.changes === 0) {
+            resolve(null); // if member is not found
+          } else {
+            resolve(id);
+          }
         }
-      }
-    );
+      );
+    }
   });
 };
 
 // Deleting a member
 const deleteAMember = (id) => {
   return new Promise((resolve, reject) => {
-    db.run('DELETE FROM Member WHERE id = ?', [id], function (error) {
-      if (error) {
-        reject(error);
-      } else if (this.changes === 0) {
-        resolve(null); // if Member is not found
-      } else {
-        resolve(id);
-      }
-    });
+    if (!id) {
+      reject('Member ID is required.');
+    } else {
+      db.run('DELETE FROM Member WHERE id = ?', [id], function (error) {
+        if (error) {
+          reject(error);
+        } else if (this.changes === 0) {
+          resolve(null); // if Member is not found
+        } else {
+          resolve(id);
+        }
+      });
+    }
   });
 };
 
-//deleting members by teamId
+// Deleting members by teamId
 const deleteMembersByTeamId = (teamId) => {
   return new Promise((resolve, reject) => {
-    db.run('DELETE FROM Member WHERE teamId = ?', [teamId], function (error) {
-      if (error) {
-        reject(error);
-      } else {
-        resolve();
-      }
-    });
+    if (!teamId) {
+      reject('Team ID is required.');
+      db.run('DELETE FROM Member WHERE teamId = ?', [teamId], function (error) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      });
+    }
   });
 };
 
--- backend/models/teamsModel.js
+++ backend/models/teamsModel.js
@@ -15,80 +15,105 @@ const getAllExistingTeams = () => {
   });
 };
 
-//Creating a new team
+// Creating a new team
 const createATeam = (data) => {
   return new Promise((resolve, reject) => {
-    db.run('INSERT INTO Team (name) VALUES (?)', [data.name], function (error) {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(this.lastID);
-      }
-    });
+    if (!data.name) {
+      reject('Team name is required.');
+    } else {
+      db.run(
+        'INSERT INTO Team (name) VALUES (?)',
+        [data.name],
+        function (error) {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(this.lastID);
+          }
+        }
+      );
+    }
   });
 };
 
 const getATeamById = (id) => {
   return new Promise((resolve, reject) => {
-    db.get('SELECT * FROM Team WHERE id = ?', [id], (error, row) => {
-      if (error) {
-        reject(error);
-      } else if (!row) {
-        resolve(null); // if there is no team with the given id
-      } else {
-        resolve(row);
-      }
-    });
+    if (!id) {
+      reject('Team ID is required.');
+    } else {
+      db.get('SELECT * FROM Team WHERE id = ?', [id], (error, row) => {
+        if (error) {
+          reject(error);
+        } else if (!row) {
+          resolve(null); // if there is no team with the given id
+        } else {
+          resolve(row);
+        }
+      });
+    }
   });
 };
 
-//get all members of team by team Id
-
+// Get all members of a team by team ID
 const getMembersByTeamId = (id) => {
   return new Promise((resolve, reject) => {
-    db.all('SELECT * FROM Member WHERE teamId = ?', [id], (error, row) => {
-      if (error) {
-        reject(error);
-      } else if (!row) {
-        resolve(null); // if there is no member in that team
-      } else {
-        resolve(row);
-      }
-    });
+    if (!id) {
+      reject('Team ID is required.');
+    } else {
+      db.all('SELECT * FROM Member WHERE teamId = ?', [id], (error, row) => {
+        if (error) {
+          reject(error);
+        } else if (!row) {
+          resolve(null); // if there are no members in that team
+        } else {
+          resolve(row);
+        }
+      });
+    }
   });
 };
 
 // Updating a team
 const updateATeam = (id, data) => {
   return new Promise((resolve, reject) => {
-    db.run(
-      'UPDATE Team SET name = ? WHERE id = ?',
-      [data.name, id],
-      function (error) {
-        if (error) {
-          reject(error);
-        } else if (this.changes === 0) {
-          resolve(null); //if there was no team found
-        } else {
-          resolve(id);
+    if (!id) {
+      reject('Team ID is required.');
+    } else if (!data.name) {
+      reject('Team name is required.');
+    } else {
+      db.run(
+        'UPDATE Team SET name = ? WHERE id = ?',
+        [data.name, id],
+        function (error) {
+          if (error) {
+            reject(error);
+          } else if (this.changes === 0) {
+            resolve(null); //if there was no team found
+          } else {
+            resolve(id);
+          }
         }
-      }
-    );
+      );
+    }
   });
 };
 
 // Delete a team
 const deleteATeam = (id) => {
   return new Promise((resolve, reject) => {
-    db.run('DELETE FROM Team WHERE id = ?', [id], function (error) {
-      if (error) {
-        reject(error);
-      } else if (this.changes === 0) {
-        resolve(null); // if a team not found
-      } else {
-        resolve(id);
-      }
-    });
+    if (!id) {
+      reject('Team ID is required.');
+    } else {
+      db.run('DELETE FROM Team WHERE id = ?', [id], function (error) {
+        if (error) {
+          reject(error);
+        } else if (this.changes === 0) {
+          resolve(null); // if a team is not found
+        } else {
+          resolve(id);
+        }
+      });
+    }
   });
 };
 
